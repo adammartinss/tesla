39206725f22c159e239f3077290a0a14d0985418
Adam Martins Santos <103488271+adammartinss@users.noreply.github.com>
2022-05-23 17:53:32 -0300

    deus abençoe

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import useWrapperScroll from '../useWrapperScroll';
 import { Container } from './styles'
 
 const ModelOverlay: React.FC = ({children}) => {
+    const scroll = useWrapperScroll()
     return(
         <Container>
             {children}
